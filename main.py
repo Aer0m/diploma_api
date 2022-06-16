9e7c681df9c725c3a0eaa827f0be0e5584eee9a0
Aer0m <47530448+Aer0m@users.noreply.github.com>
2022-06-17 02:36:52 +0300

    host+port

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def predict_iris():
   # возвращаем результат
    return 'Predicted result for observation ' + str(unseen) + ' is: ' + str(result)
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', debug=True, port=5000)
